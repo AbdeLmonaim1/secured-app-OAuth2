875ac1a2c7cde863e1a9b2fc89d39733ab7bd52e
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-02-13 22:58:32 +0100

    add appCustomer with Thymeleaf

---
+++ appcustomer-front-thymeleaf/src/main/java/ma/app/appcustomerfrontthymeleaf/AppcustomerFrontThymeleafApplication.java
@@ -0,0 +1,13 @@
+package ma.app.appcustomerfrontthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcustomerFrontThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppcustomerFrontThymeleafApplication.class, args);
+    }
+
+}
---
+++ appcustomer-front-thymeleaf/src/test/java/ma/app/appcustomerfrontthymeleaf/AppcustomerFrontThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.app.appcustomerfrontthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcustomerFrontThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
