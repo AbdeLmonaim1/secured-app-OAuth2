555e45192db6d1bbdf88839c96133911dd1f3f0d
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-02-15 03:31:18 +0100

    Configurate authentication via Keycloack Oauth2 provider

--- appcustomer-front-thymeleaf/src/main/java/ma/app/appcustomerfrontthymeleaf/security/SecurityConfig.java
+++ appcustomer-front-thymeleaf/src/main/java/ma/app/appcustomerfrontthymeleaf/security/SecurityConfig.java
@@ -1,14 +1,23 @@
 package ma.app.appcustomerfrontthymeleaf.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private ClientRegistrationRepository clientRegistrationRepository;
+    public SecurityConfig(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(Customizer.withDefaults())
@@ -22,4 +31,5 @@ public class SecurityConfig {
                         .deleteCookies("JSESSIONID"))
                 .build();
     }
+
 }
