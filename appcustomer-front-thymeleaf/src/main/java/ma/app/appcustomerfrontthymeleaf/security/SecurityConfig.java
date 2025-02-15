80e93140bb8d57ac7d7d6ef0ad505b4eed2f76d7
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-02-15 22:28:28 +0100

    Authentication with three Oauth2 providers Google, Github and Keycloack

--- appcustomer-front-thymeleaf/src/main/java/ma/app/appcustomerfrontthymeleaf/security/SecurityConfig.java
+++ appcustomer-front-thymeleaf/src/main/java/ma/app/appcustomerfrontthymeleaf/security/SecurityConfig.java
@@ -3,18 +3,31 @@ package ma.app.appcustomerfrontthymeleaf.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
+//Pour activer la security avec les roles
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
-    private ClientRegistrationRepository clientRegistrationRepository;
-    public SecurityConfig(ClientRegistrationRepository clientRegistrationRepository) {
-        this.clientRegistrationRepository = clientRegistrationRepository;
-    }
+    //Les trois client(registration) Oauth2 (Gmail, Github, Keycloack) sont stockes par Spring Security dans un objet de type ClientRegistrationRepository
+//    private ClientRegistrationRepository clientRegistrationRepository;
+//    public SecurityConfig(ClientRegistrationRepository clientRegistrationRepository) {
+//        this.clientRegistrationRepository = clientRegistrationRepository;
+//    }
 
 
     @Bean
@@ -22,14 +35,44 @@ public class SecurityConfig {
         return http
                 .csrf(Customizer.withDefaults())
                 //Cette ressources ne necessaiste pas une authentification
-                .authorizeHttpRequests(ar->ar.requestMatchers("/","/webjars/**").permitAll())
+                .authorizeHttpRequests(ar->ar.requestMatchers("/","/oauth2Login/**","/webjars/**").permitAll())
                 //Toutes les requetes necessaiste une authentification
                 .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login(ol->ol.loginPage("/oauth2Login"))
                 .logout(logout-> logout
+//                        .logoutSuccessHandler(oidcLogoutSuccessHandler())
                         .logoutSuccessUrl("/").permitAll()
+//                        .clearAuthentication(true)
                         .deleteCookies("JSESSIONID"))
+                .exceptionHandling(eh->eh.accessDeniedPage("/notAuthorized"))
                 .build();
     }
-
+//    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+//        final OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
+//                new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
+//        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/logoutsuccess=true");
+//        return oidcLogoutSuccessHandler;
+//    }
+    @Bean
+    public GrantedAuthoritiesMapper userAuthoritiesMapper() {
+        //les authorites existe dans le context de Security car on a deux types de utilisateurs (OAUTH2_USER et OIDC_USER)
+        return authorities -> {
+            //Collection des roles
+            final Set<GrantedAuthority> mapperAuthorities = new HashSet<>();
+            authorities.forEach((authority) ->{
+                if (authority instanceof OidcUserAuthority oidcUserAuthority) {
+                    mapperAuthorities.addAll(mapAuthorities(oidcUserAuthority.getIdToken().getClaims()));
+                    System.out.println(oidcUserAuthority);
+                } else if (authority instanceof OAuth2UserAuthority oauth2UserAuthority) {
+                    mapperAuthorities.addAll(mapAuthorities(oauth2UserAuthority.getAttributes()));
+                }
+            });
+                return mapperAuthorities;
+        };
+    }
+    public List<SimpleGrantedAuthority> mapAuthorities(final Map<String, Object> attributes) {
+        final Map<String, Object> realmsAccess = (Map<String, Object>) attributes.getOrDefault("realm_access", Collections.emptyMap());
+        final Collection<String> roles = (Collection<String>) realmsAccess.getOrDefault("roles", Collections.emptyList());
+        return roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
 }
--- appcustomer-front-thymeleaf/src/main/java/ma/app/appcustomerfrontthymeleaf/web/CustomerController.java
+++ appcustomer-front-thymeleaf/src/main/java/ma/app/appcustomerfrontthymeleaf/web/CustomerController.java
@@ -1,19 +1,30 @@
 package ma.app.appcustomerfrontthymeleaf.web;
 
 import ma.app.appcustomerfrontthymeleaf.repository.CustomerRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class CustomerController {
+    private final ClientRegistrationRepository clientRegistrationRepository;
     private CustomerRepository customerRepository;
-    public CustomerController(CustomerRepository customerRepository) {
+    public CustomerController(CustomerRepository customerRepository, ClientRegistrationRepository clientRegistrationRepository) {
         this.customerRepository = customerRepository;
+        this.clientRegistrationRepository = clientRegistrationRepository;
     }
     @GetMapping("/customers")
+    //Dans un premier temps sa marche pas car le JWT ne contient pas des roles
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String customers(Model model) {
         model.addAttribute("customers", customerRepository.findAll());
         return "customers";
@@ -36,5 +47,19 @@ public class CustomerController {
     public String index(){
         return "index";
     }
-
+    @GetMapping("/notAuthorized")
+    public String notAuthorized(){
+        return "notAuthorized";
+    }
+    @GetMapping("/oauth2Login")
+    public String oauth2Login(Model model){
+        String authorizationRequestBaseUri = "oauth2/authorization";
+        Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+        Iterable<ClientRegistration> clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
+        clientRegistrations.forEach(registration -> {
+            oauth2AuthenticationUrls.put(registration.getClientName(), authorizationRequestBaseUri+"/"+registration.getRegistrationId());
+        });
+        model.addAttribute("urls", oauth2AuthenticationUrls);
+        return "oauth2Login";
+    }
 }
